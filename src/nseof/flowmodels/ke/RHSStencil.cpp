a7074c6e8f32f79cbaf82ea1a05b43622d0a2764
josefWinter <josefwinter@linux-pxyf.suse>
2016-01-16 22:33:37 +0100

    adapted k-epsilon for 3D and changed implementation for keloop counter

--- src/nseof/flowmodels/ke/RHSStencil.cpp
+++ src/nseof/flowmodels/ke/RHSStencil.cpp
@@ -67,7 +67,55 @@ void RHSStencil::apply(FlowField& flowField, int i, int j) {
 }
 
 void RHSStencil::apply(FlowField& flowField, int i, int j, int k) {
-  // TODO: 3D
+  //  loadLocalNu3D(_parameters, flowField, _localNu, i, j, k);
+  loadLocalVelocity3D(flowField, _localVelocity, i, j, k);
+  loadLocalMeshsize3D(_parameters, _localMeshsize, i, j, k);
+
+  // load TKE
+  loadLocal3D([&flowField](FLOAT* local, int ii, int jj, int kk) mutable {
+    *(local + 0) = flowField.getTke(ii, jj, kk);
+  }, _localTKE, i, j, k);
+
+  // load epsilon
+  loadLocal3D([&flowField](FLOAT* local, int ii, int jj, int kk) mutable {
+    *(local + 0) = flowField.getEpsilon(ii, jj, kk);
+  }, _localEpsilon, i, j, k);
+
+  // load Fmu*Nut
+  loadLocal3D([&flowField](FLOAT* local, int ii, int jj, int kk) mutable {
+    *(local + 0) = flowField.getFmu(ii, jj, kk) * flowField.getNu(ii, jj, kk);
+    //    *(local+0) = 0;
+  }, _localFmuNut, i, j, k);
+
+  const FLOAT sijsij = computeSijSij3D(_localVelocity, _localMeshsize);
+  const FLOAT f1 = flowField.getF1(i, j, k);
+  const FLOAT f2 = flowField.getF2(i, j, k);
+  const FLOAT f3 = flowField.getF3(i, j, k);
+  const FLOAT D = flowField.getD(i, j, k);
+  const FLOAT E = flowField.getE(i, j, k);
+  const FLOAT nut = flowField.getNu(i, j, k);
+
+  flowField.getsijsij(i, j, k) = sijsij;
+
+  // load nu + nut/sigmaK
+  loadLocal3D([&flowField, this](FLOAT* local, int ii, int jj, int kk) mutable {
+    *(local + 0) = this->_parameters.flow.visc +
+                   flowField.getNu(ii, jj, kk) / this->_parameters.kEpsilon.sigmaK;
+  }, _localNu, i, j, k);
+
+  flowField.getRHSTke(i, j, k) =
+      computeRHStke(_parameters, _localVelocity, _localMeshsize, _localNu,
+                    _localTKE, _localEpsilon, nut, sijsij, f3, D);
+
+  // load nu + nut/sigmaE
+  loadLocal3D([&flowField, this](FLOAT* local, int ii, int jj, int kk) mutable {
+    *(local + 0) = this->_parameters.flow.visc +
+                   flowField.getNu(ii, jj, kk) / this->_parameters.kEpsilon.sigmaE;
+  }, _localNu, i, j, k);
+
+  flowField.getRHSEpsilon(i, j, k) =
+      computeRHSepsilon(_parameters, _localVelocity, _localMeshsize, _localNu,
+                        _localTKE, _localEpsilon, nut, sijsij, f1, f2, E);
 }
 }
 }
